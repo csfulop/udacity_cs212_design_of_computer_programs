3b613fc10c64587ca8afaec1b22adc7fc408d4cf
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-03-09 14:07:08 +0100

    Quiz solution: hold_at strategy

--- pig/03_hold_at_strategy.py
+++ pig/03_hold_at_strategy.py
@@ -6,22 +6,38 @@
 # hold_at is NOT the strategy function itself). The returned
 # strategy should hold if and only if pending >= x or if the
 # player has reached the goal.
+from hamcrest.core import assert_that
+from hamcrest.core.core import is_
+
+GOAL = 50
+ROLL = 'roll'
+HOLD = 'hold'
+
 
 def hold_at(x):
     """Return a strategy that holds if and only if
     pending >= x or player reaches goal."""
+
     def strategy(state):
-        # your code here
-        pass
+        (p, me, you, pending) = state
+        if me + pending >= GOAL or pending >= x:
+            return HOLD
+        else:
+            return ROLL
+
     strategy.__name__ = 'hold_at(%d)' % x
     return strategy
 
+
 goal = 50
+
+
 def test():
-    assert hold_at(30)((1, 29, 15, 20)) == 'roll'
-    assert hold_at(30)((1, 29, 15, 21)) == 'hold'
-    assert hold_at(15)((0, 2, 30, 10))  == 'roll'
-    assert hold_at(15)((0, 2, 30, 15))  == 'hold'
+    assert_that(hold_at(30)((1, 29, 15, 20)), is_('roll'))
+    assert_that(hold_at(30)((1, 29, 15, 21)), is_('hold'))
+    assert_that(hold_at(15)((0, 2, 30, 10)), is_('roll'))
+    assert_that(hold_at(15)((0, 2, 30, 15)), is_('hold'))
     return 'tests pass'
+
 
 print(test())
