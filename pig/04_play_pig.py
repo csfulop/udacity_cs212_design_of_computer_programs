7c3957e0f150fce3311d8a0cf7d73a66452a38ff
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-03-09 14:08:11 +0100

    Quiz solution: play pig

--- pig/04_play_pig.py
+++ pig/04_play_pig.py
@@ -9,21 +9,24 @@
 
 import random
 
-possible_moves = ['roll', 'hold']
-other = {1: 0, 0: 1}
-goal = 50
+HOLD = 'hold'
+ROLL = 'roll'
+POSSIBLE_MOVES = [ROLL, HOLD]
+OTHER = {1: 0, 0: 1}
+GOAL = 50
+TURN_LIMIT = 1000
 
 
 def clueless(state):
     "A strategy that ignores the state and chooses at random from possible moves."
-    return random.choice(possible_moves)
+    return random.choice(POSSIBLE_MOVES)
 
 
 def hold(state):
     """Apply the hold action to a state to yield a new state:
     Reap the 'pending' points and it becomes the other player's turn."""
     (p, me, you, pending) = state
-    return (other[p], you, me + pending, 0)
+    return (OTHER[p], you, me + pending, 0)
 
 
 def roll(state, d):
@@ -32,7 +35,7 @@ def roll(state, d):
     and it is the other player's turn. If d > 1, add d to 'pending' points."""
     (p, me, you, pending) = state
     if d == 1:
-        return (other[p], you, me + 1, 0)  # pig out; other player's turn
+        return (OTHER[p], you, me + 1, 0)  # pig out; other player's turn
     else:
         return (p, me, you, pending + d)  # accumulate die roll in pending
 
@@ -42,15 +45,26 @@ def play_pig(A, B):
     Each time through the main loop we ask the current player for one decision,
     which must be 'hold' or 'roll', and we update the state accordingly.
     When one player's score exceeds the goal, return that player."""
-    # your code here
+    state = (0, 0, 0, 0)
+    for i in range(TURN_LIMIT):
+        (p, me, you, pending) = state
+        if me >= GOAL:
+            return B if p else A
+        action = B(state) if p else A(state)
+        if action == HOLD:
+            state = hold(state)
+        elif action == ROLL:
+            state = roll(state, random.randint(1, 6))
+        else:
+            raise Exception('Invalid action:' + action)
 
 
 def always_roll(state):
-    return 'roll'
+    return ROLL
 
 
 def always_hold(state):
-    return 'hold'
+    return HOLD
 
 
 def test():
@@ -61,4 +75,3 @@ def test():
 
 
 print(test())
-
