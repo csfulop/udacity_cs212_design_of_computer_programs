8e0ead7730397d83e984c7264e38d7a667c64dcd
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-03-30 21:40:09 +0100

    Quiz solution: parking lot search

--- 07_exam/04_parking_lot_search.py
+++ 07_exam/04_parking_lot_search.py
@@ -80,7 +80,7 @@ pairs, where each pair is a tuple and the locations are a tuple.  Here is the
 initial state for the problem above in this format:
 """
 
-puzzle0 = (
+puzzle1a = (
     ('@', (31,)),
     ('*', (26, 27)),
     ('G', (9, 10)),
@@ -110,6 +110,7 @@ def solve_parking_puzzle(start, N=N):
     of (object, locations) pairs).  Return a path of [state, action, ...]
     alternating items; an action is a pair (object, distance_moved),
     such as ('B', 16) to move 'B' two squares down on the N=8 grid."""
+    return shortest_path_search(start, successors, is_goal)
 
 
 # But it would also be nice to have a simpler format to describe puzzles,
@@ -118,6 +119,7 @@ def solve_parking_puzzle(start, N=N):
 
 def locs(start, n, incr=1):
     "Return a tuple of n locations, starting at start and incrementing by incr."
+    return tuple(start + i for i in range(0, n * incr, incr))
 
 
 def grid(cars, N=N):
@@ -128,6 +130,15 @@ def grid(cars, N=N):
     pair, like ('@', (31,)), to indicate this. The variable 'cars'  is a
     tuple of pairs like ('*', (26, 27)). The return result is a big tuple
     of the 'cars' pairs along with the walls and goal pairs."""
+    exit = (N // 2 - 1) * N + N - 1
+    board = [('@', (exit,))]
+    board.extend(list(cars))
+    boarders = [y * N + x
+                for y in range(N)
+                for x in range(N)
+                if y in (0, N - 1) or x in (0, N - 1) and y * N + x != exit]
+    board.append(('|', tuple(boarders)))
+    return tuple(board)
 
 
 def show(state, N=N):
@@ -139,7 +150,7 @@ def show(state, N=N):
             board[s] = c
     # Now print it out
     for i, s in enumerate(board):
-        print(s,)
+        print(s, end=' ')
         if i % N == N - 1: print()
 
 
@@ -152,6 +163,24 @@ puzzle1 = grid((
     ('P', locs(17, 3, N)),
     ('O', locs(41, 2, N)),
     ('B', locs(20, 3, N)),
+    ('A', locs(45, 2))))
+
+puzzle1_G1 = grid((
+    ('*', locs(26, 2)),
+    ('G', locs(10, 2)),
+    ('Y', locs(14, 3, N)),
+    ('P', locs(17, 3, N)),
+    ('O', locs(41, 2, N)),
+    ('B', locs(20, 3, N)),
+    ('A', locs(45, 2))))
+
+puzzle1_Bm8 = grid((
+    ('*', locs(26, 2)),
+    ('G', locs(9, 2)),
+    ('Y', locs(14, 3, N)),
+    ('P', locs(17, 3, N)),
+    ('O', locs(41, 2, N)),
+    ('B', locs(12, 3, N)),
     ('A', locs(45, 2))))
 
 puzzle2 = grid((
@@ -168,13 +197,19 @@ puzzle3 = grid((
     ('O', locs(45, 2, N)),
     ('Y', locs(49, 3))))
 
+puzzle4done = grid((
+    ('*', locs(30, 2)),
+))
+
 
 # Here are the shortest_path_search and path_actions functions from the unit.
 # You may use these if you want, but you don't have to.
 
 def shortest_path_search(start, successors, is_goal):
     """Find the shortest path from start state to a state
-    such that is_goal(state) is true."""
+    such that is_goal(state) is true.
+    successor: (state) -> Dict[state,action]
+    is_goal: (state) -> bool"""
     if is_goal(start):
         return [start]
     explored = set()  # set of states we have visited
@@ -197,3 +232,113 @@ def path_actions(path):
     "Return a list of actions in this path."
     return path[1::2]
 
+
+def is_goal(state):
+    s = _state_dict(state)
+    return s['@'][0] in s['*']
+
+
+def _state_dict(state):
+    return {k: v for (k, v) in state}
+
+
+def successors(state):
+    result = {}
+    actions = _available_actions(state)
+    for action in actions:
+        result[_apply_action(state, action)] = action
+    return result
+
+
+def _available_actions(state):
+    actions = []
+    s = _state_dict(state)
+    for car, pos in s.items():
+        if car in ('@', '|'):
+            continue
+        increment = pos[1] - pos[0]
+        move = -increment
+        while pos[0] + move > 0 and _is_free(state, pos[0] + move):
+            actions.append((car, move))
+            move -= increment
+        move = increment
+        while pos[-1] + move <= max(s['|']) and _is_free(state, pos[-1] + move):
+            actions.append((car, move))
+            move += increment
+    return actions
+
+
+def _is_free(state, pos):
+    for item in state:
+        if item[0] == '@': continue
+        if pos in item[1]: return False
+    return True
+
+
+def _apply_action(state, action):
+    result = []
+    for item in state:
+        if action[0] == item[0]:
+            result.append((item[0], tuple(pos + action[1] for pos in item[1])))
+        else:
+            result.append(item)
+    return tuple(result)
+
+
+def tests():
+    assert locs(26, 2) == (26, 27)
+    assert locs(20, 3, N) == (20, 28, 36)
+
+    show(puzzle1)
+    assert puzzle1a == puzzle1
+
+    assert _state_dict(puzzle1) == {
+        '@': (31,),
+        '*': (26, 27),
+        'G': (9, 10),
+        'Y': (14, 22, 30),
+        'P': (17, 25, 33),
+        'O': (41, 49),
+        'B': (20, 28, 36),
+        'A': (45, 46),
+        '|': (0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 23, 24, 32, 39, 40, 47, 48, 55, 56, 57, 58, 59, 60, 61, 62, 63)
+    }
+
+    assert is_goal(puzzle1) == False
+    assert is_goal(puzzle4done) == True
+
+    assert _is_free(puzzle1, 9) == False
+    assert _is_free(puzzle1, 11) == True
+    assert _is_free(puzzle1, 8) == False
+    assert _is_free(puzzle1, 1) == False
+    assert _is_free(puzzle1, 31) == True
+
+    assert set(_available_actions(puzzle1)) == \
+           {('G', 1), ('G', 2), ('G', 3), ('Y', 8), ('B', -8), ('B', 8), ('B', 16), ('A', -1), ('A', -2), ('A', -3)}
+
+    assert _apply_action(puzzle1, ('G', 1)) == puzzle1_G1
+    assert _apply_action(puzzle1, ('B', -8)) == puzzle1_Bm8
+
+    succ = successors(puzzle1)
+    assert sorted(list(succ.values())) == sorted(_available_actions(puzzle1))
+    ok_G1 = ok_Bm8 = False
+    for (state, action) in successors(puzzle1).items():
+        if action == ('G', 1):
+            assert state == puzzle1_G1
+            ok_G1 = True
+        if action == ('B', -8):
+            assert state == puzzle1_Bm8
+            ok_Bm8 = True
+    assert ok_G1
+    assert ok_Bm8
+
+    result = shortest_path_search(puzzle1, successors, is_goal)
+    assert path_actions(result) == [('A', -3), ('Y', 24), ('B', 16), ('*', 4)]
+    assert path_actions(solve_parking_puzzle(puzzle1)) == [('A', -3), ('Y', 24), ('B', 16), ('*', 4)]
+
+    assert path_actions(solve_parking_puzzle(puzzle4done)) == []
+
+    return 'tests pass'
+
+
+print(tests())
